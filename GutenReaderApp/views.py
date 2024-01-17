3e5b90b297c50dc3c255714c13678b11d7b15a73
Milad <mmasoodi@uwm.edu>
2024-01-16 19:12:58 -0600

    home.html and its view

--- GutenReaderApp/views.py
+++ GutenReaderApp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from models import Book
+from django.views import View
+
 
 # Create your views here.
+class Home(View):
+    def get(self, request):
+        books = list(Book.objects.all())
+        return render(request, "home.html", {"books": books})
