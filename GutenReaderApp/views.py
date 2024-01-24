667fffbf056b1819ed7017c71daff9448c63a167
Milad <mmasoodi@uwm.edu>
2024-01-23 20:07:43 -0600

    Home to index links and index view

--- GutenReaderApp/views.py
+++ GutenReaderApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from GutenReaderApp.models import Book
 from django.views import View
 
@@ -10,5 +10,8 @@ class Home(View):
         return render(request, "home.html", {"books": books})
 class Index(View):
     def get(self, request, book_id):
-        current_book = Book.objects.get(id=book_id)
+        current_book = get_object_or_404(Book, pk=book_id)
 
+        context = {'Title': current_book.Title, 'Author': current_book.Author, 'Language': current_book.Language,
+                   'Chap_Titles': current_book.chapter_titles}
+        return render(request, "bookIndex.html", context)
