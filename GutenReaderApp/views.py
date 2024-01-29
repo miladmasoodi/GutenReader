95c53b7f85fecc00a10f5fbd0278f44efc5818ed
Milad <mmasoodi@uwm.edu>
2024-01-29 11:53:39 -0600

    book_index.html and views fixes

--- GutenReaderApp/views.py
+++ GutenReaderApp/views.py
@@ -8,10 +8,12 @@ class Home(View):
     def get(self, request):
         books = list(Book.objects.all())
         return render(request, "home.html", {"books": books})
+
+
 class Index(View):
     def get(self, request, book_id):
         current_book = get_object_or_404(Book, pk=book_id)
 
-        context = {'Title': current_book.Title, 'Author': current_book.Author, 'Language': current_book.Language,
+        context = {'Title': current_book.title, 'Author': current_book.author, 'Language': current_book.language,
                    'Chap_Titles': current_book.chapter_titles}
-        return render(request, "bookIndex.html", context)
+        return render(request, "book_index.html", context)
