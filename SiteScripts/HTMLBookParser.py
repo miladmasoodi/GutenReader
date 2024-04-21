0da6b04e57c299f47b88aff34b1d3ace23f01f12
Milad <mmasoodi@uwm.edu>
2024-04-20 20:52:08 -0500

    SubjectTag models, get_meta_tags, pg_id logic, add_subject_tags, admin registration, migration

--- SiteScripts/HTMLBookParser.py
+++ SiteScripts/HTMLBookParser.py
@@ -2,6 +2,7 @@ import unicodedata
 
 
 def get_section_indices(chapter_divisions):
+    # assume tiny chaps are book or section titles
     section_indices = []
     for i in range(len(chapter_divisions) - 1, 0, -1):
         line_difference = chapter_divisions[i] - chapter_divisions[i - 1]
@@ -12,10 +13,10 @@ def get_section_indices(chapter_divisions):
 
 
 def parse_html_file(html_file):
+    # parse desired data from a gutenburg project book html file
+    # return: dict of: meta_values, full_text, chap_titles, chap_starts, meta_tags
     START_TEXT = "*** START OF"
     END_TEXT = "*** END OF"
-
-    # assume tiny chaps are book or section titles
 
     toc_lines = []
     chap_titles = []
@@ -46,10 +47,20 @@ def parse_html_file(html_file):
         start_position = sample_position + offset
         end_position = meta_portion[start_position:].find("<") + start_position
         meta_values[index] = meta_portion[start_position:end_position]
-
     print(meta_values)
-    end_of_toc = find_line_of_value(lines, "<!--end chapter-->")
 
+    SUBJECT_SAMPLE = '<meta name="dc.subject" content="'
+    meta_tag_start = find_line_of_value(lines, SUBJECT_SAMPLE)
+    meta_portion = lines[meta_tag_start:start_line]  # same values but as a list
+    meta_tags = get_meta_tags(SUBJECT_SAMPLE, meta_portion)
+
+    PG_ID_SAMPLE = '<meta property="og:url" content="https://www.gutenberg.org/ebooks/'
+    pg_id_line = find_line_of_value(lines, PG_ID_SAMPLE)
+    offset = len(PG_ID_SAMPLE)
+    end_position = lines[pg_id_line][offset:].find("/") + offset
+    pg_id = lines[pg_id_line][offset:end_position]
+
+    end_of_toc = find_line_of_value(lines, "<!--end chapter-->")
     toc_lines = find_all_lines_of_value(lines[:end_of_toc], 'href="#')
 
     # to see if it started counting non-toc <a>'s
@@ -58,26 +69,36 @@ def parse_html_file(html_file):
     chap_ids = find_id_values(lines, toc_lines)
     chap_starts = find_ch_start_lines(lines, chap_ids)
     chap_titles = find_chap_titles(lines, toc_lines)
-    print(chap_titles)
-    print(chap_ids)
-    print(chap_starts)
+    # print(chap_titles)
+    # print(chap_ids)
+    # print(chap_starts)
     if len(chap_ids) < len(chap_starts):
         raise Exception("too many chap_starts")
 
     chap_starts.append(end_line)
-    # # split into diff strings before reformating so chap positions aren't lost
-    # chap_portions = []
-    # prev_index = 0
-    # for i in range(1, len(chap_starts)):
-    #     chap_portions.append(lines[chap_starts[prev_index]:chap_starts[i]])
-    #     prev_index = i
+    print(meta_tags)
 
-    # new_book = my_models.Book(title=meta_values[0], author=meta_values[1], language=meta_values[2],
-    #                           full_text='\n'.join(lines), chapter_titles=chap_titles, chapter_divisions=chap_starts)
-    # new_book.save()
     book_dict = {"meta_values": meta_values, "full_text": '\n'.join(lines),
-                 "chapter_titles": chap_titles, "chapter_divisions": chap_starts}
+                 "chapter_titles": chap_titles, "chapter_divisions": chap_starts,
+                 "meta_tags": meta_tags, "pg_id": pg_id}
     return book_dict
+
+
+def get_meta_tags(sample, meta_portion):
+    offset = len(sample)
+    subject_lines = find_all_lines_of_value(meta_portion, sample)
+    meta_tags = []
+    for line in subject_lines:
+        sample_position = meta_portion[line].find(sample)
+        start_position = sample_position + offset
+        end_position = meta_portion[line][start_position:].find('"') + start_position
+        end = " -- "
+        tag_string = meta_portion[line][start_position:end_position]
+        tag_string = tag_string[:30]
+        tag_cutoff = tag_string.find(end)
+        if tag_cutoff != -1:
+            meta_tags.append(tag_string[:tag_cutoff])
+    return meta_tags
 
 
 def revise_toc_lines(toc_lines):
