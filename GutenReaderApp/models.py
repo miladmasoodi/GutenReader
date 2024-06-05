dcbf660df3dd5b56b5b55e8b3ba90b52b80aac74
Milad <mmasoodi@uwm.edu>
2024-06-05 14:02:50 -0500

    initial cover image parsing

--- GutenReaderApp/models.py
+++ GutenReaderApp/models.py
@@ -4,7 +4,10 @@ from django.dispatch import receiver
 from django.utils import timezone
 from SiteScripts import HTMLBookParser
 from mimetypes import guess_type
+from zipfile import ZipFile, is_zipfile
 import os
+import re
+from django.core.files import File
 
 """File field, Text field(contents of .txt), 
 Meta - Title, Author, Language: Char fields(3) 
@@ -24,7 +27,9 @@ class Book(models.Model):
     date_added = models.DateTimeField(default=timezone.now)
     project_gutenberg_id = models.IntegerField(default=-1)
 
-    # Content Info
+    # Content
+    #default cover image isn't indivdualy saved in DB
+    book_cover = models.ImageField(upload_to="book_covers/", blank=True, null=True)
     full_text = models.TextField()
     # Chapter Info
     chapter_titles = models.JSONField(default=list)
@@ -46,16 +51,61 @@ class SubjectTag(models.Model):
     def __str__(self):
         return self.content
 
+def has_dir(zip_file):
+    info_list = zip_file.infolist()
+    for info in info_list:
+        if info.is_dir():
+            return True
+    return False
+
 
 @receiver(post_save, sender=TextUpload)  # uses signals
 def parse_book(sender, instance, created, **kwargs):
     if created:
+        has_cover = False
+        cover_path = ""
         cur_book_file = instance.book_file
-        context_type = guess_type(cur_book_file.path)[0]
-        if context_type != "text/html":
-            raise Exception("HTML Uploads Only: " + str(context_type))
+        path = cur_book_file.path
+        context_type = guess_type(path)[0]
+        is_zip = context_type == "application/zip"
+        is_html = context_type == "text/html"
+        if not (is_html or (is_zip and is_zipfile(path))):
+            raise Exception("HTML or ZIP Uploads Only: " + str(context_type))
+        if is_zip:
+            cover_regex = r"\S*cover.\w+"
+            html_regex = r"\S*.htm\w?"
 
-        f = open(cur_book_file.path, "r", encoding='utf-8')
+            with ZipFile("zip_test.zip", 'r') as zip:
+                x = zip.testzip()
+                info_list = zip.infolist()
+                name_list = zip.namelist()
+                cover_match_count = 0
+                cover_match = ""
+                html_match_count = 0
+                html_match = ""
+                print(name_list)
+                for name in name_list:
+                    cover_match = re.search(cover_regex, name)
+                    if cover_match is not None:
+                        print(cover_match.string)
+                        cover_match_count += 1
+                        cover_match = cover_match.string
+                    html_match = re.search(html_regex, name)
+                    if html_match is not None:
+                        print(html_match.string)
+                        html_match_count += 1
+                        html_match = html_match.string
+                if html_match_count != 1:
+                    raise Exception("Html not found")
+                if cover_match_count != 1:
+                    raise Exception("Cover not found")
+                cover_path = zip.extract(cover_match, "book_covers/")
+                print(cover_path)
+                html_path = zip.extract(html_match)
+                print(html_path)
+                path = html_path
+
+        f = open(path, "r", encoding='utf-8')
         try:
             result = HTMLBookParser.parse_html_file(f)
         except Exception as e:
@@ -70,18 +120,23 @@ def parse_book(sender, instance, created, **kwargs):
                             chapter_divisions=result["chapter_divisions"],
                             section_indices=result["section_indices"],
                             project_gutenberg_id=result["pg_id"])
+            if has_cover:
+                local_file = open(cover_path, "rb")
+                django_file = File(local_file)
+                new_book.book_cover.save('Cover.jpg', django_file)
+                local_file.close()
             new_book.save()
             add_subject_tags(new_book, result["meta_tags"])
         f.close()
 
-        os.remove(cur_book_file.path)
+        os.remove(path)
         instance.delete()
 
 
 def add_subject_tags(book, subject_tags: list):
     for tag in subject_tags:
-        tag_model, tag_bool = SubjectTag.objects.get_or_create(content=tag)
+        tag_model, tag_created = SubjectTag.objects.get_or_create(content=tag)
         tag_model.books.add(book)
         tag_model.save()
-        if tag_bool:
+        if tag_created:
             print("NEW Tag: " + str(tag_model))
